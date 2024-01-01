42de056c408ddf4cb7cb7eb3540ede63f2674275
Saumya Shah <144374729+SaumyaShah0@users.noreply.github.com>
2024-01-01 19:12:37 +0530

    Update Footer.js
    19:12
    1 JAN 2024

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,14 @@
 import React from "react";
 
 export const Footer = () => {
+  let footerStyle = {
+    position: "relative",
+    top: "95vh",
+    width: "100%",
+  }
   return (
     <>
-      <footer className="bg-body-tertiary text-center text-lg-start">
+      <footer className="bg-body-tertiary text-center text-lg-start" style={footerStyle}>
         <div
           className="text-center p-3"
           //   style="background-color: rgba(0, 0, 0, 0.05);"
