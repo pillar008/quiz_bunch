45ef3e1ac86974a0492560207fce2e66eb5deef1
pillar008 <84141574+pillar008@users.noreply.github.com>
2024-01-01 18:22:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+import facebook from "./facebook.svg";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <>
+      <footer className="bg-body-tertiary text-center text-lg-start">
+        <div
+          className="text-center p-3"
+          //   style="background-color: rgba(0, 0, 0, 0.05);"
+        >
+          © 2020 Copyright:
+          <a className="text-body" href="https://mdbootstrap.com/">
+            MDBootstrap.com
+          </a>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
